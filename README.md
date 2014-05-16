7da051aa15d3344def942b6db99c39dd10c426d0
yoggy <yoggy0@gmail.com>
2014-05-16 20:59:56 +0900

    added : push-pull model

---
+++ README.md
@@ -0,0 +1,25 @@
+zeromq-learning
+========
+
+pub-sub model
+  * [src/zeromq-pub/zeromq-pub.cpp](src/zeromq-pub/zeromq-pub.cpp)
+  * [src/zeromq-sub/zeromq-sub.cpp](src/zeromq-sub/zeromq-sub.cpp)
+  * publishしたデータはすべてのsubscriberが受信するモデル
+  * pub・subどちらもlisten側になることができる
+  * pub・subどちら側から起動してもOK
+  * pub・subのどちら側を再起動しても再接続される
+
+push-pull model
+  * [src/zeromq-push/zeromq-push.cpp](src/zeromq-push/zeromq-push.cpp)
+  * [src/zeromq-pull/zeromq-pull.cpp](src/zeromq-pull/zeromq-pull.cpp)
+  * pushしたデータはどれか一つのpull側が受信するモデル
+  * push・pullどちらもlisten側になることができる
+    * push側がlistenしている場合は、誰かpullするまでzmq_send()はブロックする。
+    * pull側がlistenしている場合は、pushしたメッセージは誰かがpullするまでキューイングされる
+  * push・pullどちら側から起動してもOK
+  * push・pullのどちら側を再起動しても再接続される
+
+req-res model
+  * [src/zeromq-req/zeromq-req.cpp](src/zeromq-req/zeromq-req.cpp)
+  * [src/zeromq-res/zeromq-res.cpp](src/zeromq-res/zeromq-res.cpp)
+
---
+++ src/zeromq-push/zeromq-push.cpp
@@ -0,0 +1,60 @@
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "zmq.h"
+#if _DEBUG
+#pragma comment(lib, "libzmq-v100-mt-gd-4_0_4.lib")
+#else
+#pragma comment(lib, "libzmq-v100-mt-4_0_4.lib")
+#endif
+
+int main(int argc, char* argv[])
+{
+	void *context = zmq_ctx_new();
+	void *socket = zmq_socket(context, ZMQ_PUSH);
+	int rv = zmq_bind(socket, "tcp://*:12345");
+	if (rv != 0) {
+		printf("zmq_bind() failed...%s\n", zmq_strerror(zmq_errno()));
+		return -1;
+	}
+
+	printf("start zeromq-push...\n");
+
+	// create message...
+	char *buf = NULL;
+	int buf_size = 1024 * 1024 * 4;
+	buf = (char *)malloc(buf_size);
+	memset(buf, 0, buf_size);
+	memset(buf, 'A', buf_size-1);
+
+	DWORD t = 0;
+	while (1) {
+		DWORD st = ::GetTickCount();
+
+		memcpy(buf, &t, 4);
+
+		// publish message
+		int size = zmq_send(socket, buf, buf_size, 0);
+		printf("send : %d, %d\n", t, buf_size);
+
+		DWORD et = ::GetTickCount();
+		DWORD diff = et - st;
+		if (diff > 33) diff = 33;
+		Sleep(33 - diff);
+
+		t ++;
+		if (t%100 == 0) {
+			printf("diff = %d\n", diff);
+		}
+	}
+
+	zmq_close(socket);
+	zmq_ctx_destroy(context);
+
+	return 0;
+}
+
